a7e27a47f778c71bd4c628d064dffca08f8c74b7
br4ndy-code <naxelord@gmail.com>
2021-11-17 15:22:23 +0200

    Update

--- config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    TOKEN: '2119812922:AAGyAVoGEzPnnWIMdZ4zLuBAHUatcPPPNqs',
-    DB: './db/db.js',
-}
-
-module.exports = config;
--- random.js
+++ random.js
@@ -1,19 +1,39 @@
-function random(num){
-  let quotes = ["Success is a journey, not a destination.",
+function random(num, category){
+  let success = ["Success is a journey, not a destination.",
   "Success is where preparation and opportunity meet.",
   "Success is how high you bounce when you hit bottom.",
   "Success is the progressive realization of predetermined, worthwhile, personal goals.",
   "When it comes to success, there are no shortcuts."]
-  if (quotes.length < num){
+
+  let motivation = ["Success is a journey, not a destination.",
+  "Success is where preparation and opportunity meet.",
+  "Success is how high you bounce when you hit bottom.",
+  "Success is the progressive realization of predetermined, worthwhile, personal goals.",
+  "When it comes to success, there are no shortcuts."]
+  
+  if (success.length < num && motivation.length < num){
     return "We don't have so big amount of quotes, try again."
   }
   else{
-    let i = 0
-    while (i < num){
-      random = Math.floor(Math.random() * num);
-      console.log(quotes[random])
-      i++
+    if (category == 'motivation'){
+      categor('motivation');
     }
+    else if (category == 'success'){
+      categor('success');
+    }
+    else{
+      console.log('Wrong category');
+    }
+
+  }
+  function categor(item){
+    let i = 0
+        while (i < num){
+          r = Math.floor(Math.random() * num);
+          console.log(category[r])
+          i++
+        }
   }
 }
-export default random
+random(5,'success');
+// export default random
