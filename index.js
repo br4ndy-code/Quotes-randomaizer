8a0ff71fca5ae67183ca6913de1768bb00876e16
valek-web <ziryanovvalentin@mail.ru>
2021-11-17 15:35:24 +0300

    token

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import quotesMap from './random.js'
+import TelegramBot from 'node-telegram-bot-api'
+
+// const TelegramBot = require('node-telegram-bot-api');
+const token = '2119812922:AAGyAVoGEzPnnWIMdZ4zLuBAHUatcPPPNqs'
+
+const bot = new TelegramBot(token, {polling: true})
 
 let numberQuotes = Math.ceil(Math.random() * 10)
 
@@ -8,3 +14,8 @@ if (!numberQuotes) {
   quotesMap(numberQuotes)
   console.log(quotesMap(numberQuotes))
 }
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, quotesMap(numberQuotes));
+});
