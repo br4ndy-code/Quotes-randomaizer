9eb4882bccad4f1aad8e2277838d739ac98ad7f7
valek-web <ziryanovvalentin@mail.ru>
2021-11-19 00:22:36 +0300

    AlphaTest

--- index.js
+++ index.js
@@ -1,40 +1,50 @@
-// import quotesMap from './random.js'
-import TelegramBot from 'node-telegram-bot-api'
-import { keyboard } from './keyboard.js'
+import quotesMap from "./random.js";
+import TelegramBot from "node-telegram-bot-api";
+import { keyboard } from "./keyboard.js";
 
-const token = '2119812922:AAGyAVoGEzPnnWIMdZ4zLuBAHUatcPPPNqs'
+const token = "2119812922:AAGyAVoGEzPnnWIMdZ4zLuBAHUatcPPPNqs";
 
-const bot = new TelegramBot(token, {polling: true})
+const bot = new TelegramBot(token, { polling: true });
 
-  bot.on('message', (msg) => {
+let numberQuotes = 0;
+let category = "";
+
+bot.on("message", (msg) => {
   const chatId = msg.chat.id;
 
-  bot.sendMessage(chatId, 'Hello! What quotes do you need?', {
-        reply_markup: {
-            inline_keyboard: keyboard
-        }
+  if (Number(msg.text) > 10) {
+    bot.sendMessage(chatId, "Maximum number of citations 10! :)");
+  } else if (Number(msg.text) <= 10) {
+    numberQuotes = msg.text;
+    bot.sendMessage(chatId, "What quotes do you need?", {
+      reply_markup: {
+        inline_keyboard: keyboard,
+      },
     });
-})
+  } else {
+    bot.sendMessage(
+      chatId,
+      "Hello! Write me the required number of quotes and I will send them instantly)"
+    );
+  }
+});
 
-bot.on('callback_query', (query) => {
+bot.on("callback_query", (query) => {
   const chatId = query.message.chat.id;
 
-  let category = '';
-
-  if (query.data === 'moreMotivation') {
-    category = 'motivation';
-
+  if (query.data === "moreMotivation") {
+    category = "motivation";
   }
 
-  if (query.data === 'moreSuccess') {
-    category = 'success';
+  if (query.data === "moreSuccess") {
+    category = "success";
   }
 
   if (true) {
-    bot.sendMessage(chatId, category, {
+    bot.sendMessage(chatId, `${quotesMap(numberQuotes, category)}`, {
       reply_markup: {
-          inline_keyboard: keyboard
-      }
-  });
+        inline_keyboard: keyboard,
+      },
+    });
   }
 });
--- random.js
+++ random.js
@@ -1,44 +1,44 @@
 // Storage
-let success = ["Success is a journey, not a destination.",
+let success = [
+  "Success is a journey, not a destination.",
   "Success is where preparation and opportunity meet.",
   "Success is how high you bounce when you hit bottom.",
   "Success is the progressive realization of predetermined, worthwhile, personal goals.",
-  "When it comes to success, there are no shortcuts."]
+  "When it comes to success, there are no shortcuts.",
+];
 
-let motivation = ["Success is a journey, not a destination.",
+let motivation = [
+  "Success is a journey, not a destination.",
   "Success is where preparation and opportunity meet.",
   "Success is how high you bounce when you hit bottom.",
   "Success is the progressive realization of predetermined, worthwhile, personal goals.",
-  "When it comes to success, there are no shortcuts."]
+  "When it comes to success, there are no shortcuts.",
+];
 
 // Print the quotes
-function p_quotes(num, category){
-    let i = 0
-    while (i < num){
-      let r = Math.floor(Math.random() * num);
-      console.log(category[r])
-      i++
-    }
-  }
-
-function random(num, category){
-  
-  if (success.length < num && motivation.length < num){
-    return "We don't have so big amount of quotes, try again."
-  }
-  else{
-
-    if (category == 'motivation' || category == 'Motivation'){
-      p_quotes(num, motivation);
-    }
-    else if (category == 'success' || category == 'Success'){
-      p_quotes(num, success);
-    }
-    else{
-      console.log("Wrong option")
-    }
-
+function p_quotes(num, category) {
+  let i = 0;
+  while (i < num) {
+    let r = Math.floor(Math.random() * num);
+    i++;
+    return category[r];
   }
 }
-random(5,'success');
-// export default random
+
+function random(num, category) {
+  if (success.length < num && motivation.length < num) {
+    return "We don't have so big amount of quotes, try again.";
+  } else {
+    if (category == "motivation") {
+      return p_quotes(num, motivation);
+    } else if (category == "success") {
+      return p_quotes(num, success);
+    } else {
+      return "Wrong option";
+    }
+  }
+}
+
+console.log(random(5, "motivation"));
+
+export default random;
