16dd81cf246faa2e672a5e196be6479983809202
valek-web <ziryanovvalentin@mail.ru>
2021-11-22 21:25:11 +0300

    ButtonNumber

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import quotesMap from "./random.js";
 import TelegramBot from "node-telegram-bot-api";
-import { keyboard } from "./keyboard.js";
+import { keyboardCategory, keyboardNumber } from "./keyboard.js";
 
 const token = "2119812922:AAGyAVoGEzPnnWIMdZ4zLuBAHUatcPPPNqs";
 
@@ -11,44 +11,58 @@ let category = "";
 
 bot.on("message", (msg) => {
   const chatId = msg.chat.id;
-
-  if (Number(msg.text) > 5) {
-    bot.sendMessage(chatId, "Maximum number of citations 5! :)");
-  } else if (Number(msg.text) <= 5) {
-    numberQuotes = Number(msg.text);
-    bot.sendMessage(chatId, "What quotes do you need?", {
+  bot.sendMessage(
+    chatId,
+    "Hello! Write me the required number of quotes and I will send them instantly)",
+    {
       reply_markup: {
-        inline_keyboard: keyboard,
+        inline_keyboard: keyboardNumber,
       },
-    });
-  } else {
-    bot.sendMessage(
-      chatId,
-      "Hello! Write me the required number of quotes and I will send them instantly)"
-    );
-  }
+    }
+  );
 });
 
 const sendQuotes = (chatId) => {
   quotesMap(numberQuotes, category).map((quote) => {
     bot.sendMessage(chatId, quote);
   });
+  bot.sendMessage(
+    chatId,
+    "Write me the required number of quotes and I will send them instantly)",
+    {
+      reply_markup: {
+        inline_keyboard: keyboardNumber,
+      },
+    }
+  );
 };
 
 bot.on("callback_query", (query) => {
   const chatId = query.message.chat.id;
-
-  if (query.data === "moreMotivation") {
-    category = "motivation";
+  console.log(query.data);
+  switch (query.data) {
+    case "one": {
+      numberQuotes = 1;
+    }
+    case "two": {
+      numberQuotes = 2;
+    }
+    case "three": {
+      numberQuotes = 3;
+    }
+    case "four": {
+      numberQuotes = 4;
+    }
+    case "five": {
+      numberQuotes = 5;
+    }
+    case "moreMotivation": {
+      category = "motivation";
+    }
+    case "moreSuccess": {
+      category = "motivation";
+    }
   }
 
-  if (query.data === "moreSuccess") {
-    category = "success";
-  }
-
-  sendQuotes(chatId);
-  bot.sendMessage(
-    chatId,
-    "Write me the required number of quotes and I will send them instantly)"
-  );
+  // sendQuotes(chatId);
 });
--- keyboard.js
+++ keyboard.js
@@ -1,14 +1,46 @@
-export const keyboard = [
-    [
-      {
-        text: 'Motivation',
-        callback_data: 'moreMotivation'
-      }
-    ],
-    [
-      {
-        text: 'Success',
-        callback_data: 'moreSuccess'
-      }
-    ],
-  ];
+export const keyboardCategory = [
+  [
+    {
+      text: "Motivation",
+      callback_data: "moreMotivation",
+    },
+  ],
+  [
+    {
+      text: "Success",
+      callback_data: "moreSuccess",
+    },
+  ],
+];
+export const keyboardNumber = [
+  [
+    {
+      text: "1",
+      callback_data: "one",
+    },
+  ],
+  [
+    {
+      text: "2",
+      callback_data: "two",
+    },
+  ],
+  [
+    {
+      text: "3",
+      callback_data: "three",
+    },
+  ],
+  [
+    {
+      text: "4",
+      callback_data: "four",
+    },
+  ],
+  [
+    {
+      text: "5",
+      callback_data: "five",
+    },
+  ],
+];
